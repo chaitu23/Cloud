28a14cc87442ace0de4b6be9406324bcf323203d
chaitu23 <kvchaitanya143@gmail.com>
2019-08-13 18:17:54 +0530

    Update request3.py

--- request3.py
+++ request3.py
@@ -4,7 +4,7 @@ import requests
 def fetchData(detail):
 	url_initial='http://169.254.169.254/latest/meta-data/'
 	url_final=url_initial+detail
-	valueFetched=request.get(url_final)
+	valueFetched=requests.get(url_final)
 	return(valueFetched)
 
 if __name__ == '__main__':
